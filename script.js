d55f3a7eb38c19675c6fe322e5d10986c8c8a5f6
Bret Andersen <1bretly@gmail.com>
2023-01-12 12:24:18 -0600

    removed m

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 const friend = document.getElementById("friend")
 
-// fetch("https://evasion.cyclic.app/friends-survivors")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     data = data[Math.floor(Math.random() * data.length)]
-//     data = `${data.name}: ${data.survivor}- ${data.perks.join(", ")}`
-//     friend.innerHTML = data
-//     console.log(data)
-//   })
-
 fetch("https://evasion.cyclic.app/friends-survivors")
   .then((res) => res.json())
   .then((data) => {
+    console.log(data)
     data = data[Math.floor(Math.random() * data.length)]
     data = `${data.name}: ${data.survivor}- ${data.perks.join(", ")}`
     friend.innerHTML = data
