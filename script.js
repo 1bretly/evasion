6a21e353cf6505626d5f46b8cb7e28bfabd0bc63
Bret Andersen <1bretly@gmail.com>
2023-02-02 14:51:44 -0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const friend = document.getElementById("friend")
 
-fetch("https://evasion.cyclic.app/friends-survivors")
+fetch("https://evasion.cyclic.app/friends-killers")
   .then((res) => res.json())
   .then((data) => {
     console.log(data)
     data = data[Math.floor(Math.random() * data.length)]
-    data = `${data.name}: ${data.survivor}- ${data.perks.join(", ")}`
+    data = `${data.name}: ${data.killer}- ${data.perks.join(", ")}`
     friend.innerHTML = data
     console.log(data)
   })
